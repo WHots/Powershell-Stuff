f9226fbcd9c1e1a56385a19b788ea6f5acb7dc21
WilliamHottzz <56490828+WilliamHottzz@users.noreply.github.com>
2022-08-23 17:26:44 -0500

    Add files via upload

---
+++ Networking/Certificates/install-cert.ps1
@@ -0,0 +1,22 @@
+﻿$admin_sid = "S-1-5-32-544"
+
+
+
+
+function Is-Admin
+{
+    $var -ne (whoami /groups /fo csv | ConvertFrom-Csv | Where-Object { $_.SID -eq $admin_sid })
+    return $var
+}
+
+
+if(Is-Admin)
+{
+    Import-Certificate -FilePath .\cert.crt -CertStoreLocation Cert:\LocalMachine\Root
+}
+else
+{
+    Write-Host "Not in admin!"
+    read-host
+    Exit -1
+}
