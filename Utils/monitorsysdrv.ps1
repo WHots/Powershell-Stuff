5ad657604d51542d08441930dbf22f40b98fb9e1
Hots <56490828+WHots@users.noreply.github.com>
2024-06-01 16:06:06 -0500

    Update monitorsysdrv.ps1

--- Utils/monitorsysdrv.ps1
+++ Utils/monitorsysdrv.ps1
@@ -1,86 +1,52 @@
-#After some searching i found that this gives us the Downloads folder of the current user,
-#Appears to be working as needed.
-#$DownloadFolder = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
-
-#The system drive is default ... this code fetches the drive letter the OS is installed on, so in most case this variable = C:\
-$TargetFolder = (get-location).Drive.Name + ':\'
-#Look for all files
+$TargetFolder = (Get-Location).Drive.Name + ':\'
 $Filter = '*'
-#Monitor changes in sub dirs ... Change to $false if you want to monitor a single directory.
 $TraverseDirectory = $true
-
-$AttributeFilter = [IO.NotifyFilters]::FileName, [IO.NotifyFilters]::LastWrite,[IO.NotifyFilters]::Size, [IO.NotifyFilters]::Security
-#https://learn.microsoft.com/en-us/dotnet/api/system.io.watcherchangetypes?view=net-6.0
+$AttributeFilter = [IO.NotifyFilters]::FileName, [IO.NotifyFilters]::LastWrite, [IO.NotifyFilters]::Size, [IO.NotifyFilters]::Security
 $ChangeTypes = [System.IO.WatcherChangeTypes]::All
 
 
 
 
-function Invoke-DirectoryModified
-{
+function Invoke-DirectoryModified {
+    param (
+        [Parameter(Mandatory)]
+        [System.IO.WaitForChangedResult] $ChangeInformation
+    )
 
-  param
-  (
-    [Parameter(Mandatory)]
-    [System.IO.WaitForChangedResult]
-    $ChangeInformation
-  )
+    $changeType = $ChangeInformation.ChangeType
 
-  $var = $ChangeInformation | Select -ExpandProperty ChangeType
-  
-   switch ($var)
-   {
-        Deleted
-        {
+    switch ($changeType) {
+        'Deleted' {
             $ChangeInformation | Out-String | Write-Host -ForegroundColor Red
         }
-        Created
-        {
+        'Created' {
             $ChangeInformation | Out-String | Write-Host -ForegroundColor DarkRed
         }
-        Renamed
-        {
+        'Renamed' {
             $ChangeInformation | Out-String | Write-Host -ForegroundColor DarkYellow
         }
-        Changed
-        {
+        'Changed' {
             $ChangeInformation | Out-String | Write-Host -ForegroundColor Gray
-        } 
-   }
-
-  #Write-Warning 'Modification Caught'
-  #$ChangeInformation | Out-String | Write-Host -ForegroundColor Green
-  #echo($ChangeInformation | Format-List | Out-String)
+        }
+    }
 }
 
-
-
-
-$result = $null
-
 $watcher = New-Object -TypeName IO.FileSystemWatcher -ArgumentList $TargetFolder, $Filter -Property @{
-
     IncludeSubdirectories = $TraverseDirectory
     NotifyFilter = $AttributeFilter
 }
 
-
-try
-{
-
-    while($true)
-    {
+try {
+    while ($true) {
         $result = $watcher.WaitForChanged($ChangeTypes, 750)
 
-        if($result.TimedOut)
-        {                        
+        if ($result.TimedOut) {
             continue
         }
 
-        Invoke-DirectoryModified -Change $result
+        Invoke-DirectoryModified -ChangeInformation $result
     }
 }
-finally
-{
+finally {
     $watcher.Dispose()
 }
